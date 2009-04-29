c83291ecbbc06ad0b5e577d857e568eb32ac6b05
Max Cohan max@endpoint.com <max@v274.spreecamps.com>
2009-04-28 20:41:40 -0400

    * Further cleanup related to payment gateway refactoring

--- vendor/extensions/payment_gateway/app/controllers/admin/gateway_option_values_controller.rb
+++ vendor/extensions/payment_gateway/app/controllers/admin/gateway_option_values_controller.rb
@@ -6,13 +6,6 @@ class Admin::GatewayOptionValuesController < Admin::BaseController
   index.before do
     chosen_gateway = Gateway.find(params[:gw_id])
     @option_values = @gateway_configuration.gateway_option_values
-    unless @gateway_configuration.gateway == chosen_gateway
-      # show a blank set of values for editing purposes (original values preserved in DB until user posts the update)
-      @option_values = []
-      chosen_gateway.gateway_options.each do |option|
-        @option_values << GatewayOptionValue.new(:gateway_option => option)
-      end
-    end
   end
   
   index.response do |wants|
